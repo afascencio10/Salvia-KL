3d7c9efecf042f86be6a2a6b9ec959c954884307
Santiago Ramos <santiramos@Mac-mini-de-Santiago.local>
2026-05-25 16:30:37 -0500

    Fix import repo main.go

--- src/main.go
+++ src/main.go
@@ -150,9 +150,6 @@ func main() {
     caseDetailCtrl         := salvia_ctrl.NewCaseDetailController(caseDetailSvc, caseTimelineRepo)
     caseInfoCtrl           := salvia_ctrl.NewCaseInfoController(caseInfoSvc)
     reportCtrl             := salvia_ctrl.NewReportController(reportSvc)
-    entityLetterRepo       := repository.NewEntityLetterRepository(gormDB)
-    entityLetterSvc        := service.NewEntityLetterService(entityLetterRepo, caseTimelineRepo, caseTaskRepo)
-    entityLetterCtrl       := salvia_ctrl.NewEntityLetterController(entityLetterSvc)
     barrierV2Svc           := service.NewBarrierV2Service(barrierV2Repo)
     barrierV2GinCtrl       := salvia_ctrl.NewBarrierV2GinController(barrierV2Svc)
 
@@ -163,6 +160,10 @@ func main() {
         CaseTimelineRepo: caseTimelineRepo,
     })
     caseTaskCtrl            := salvia_ctrl.NewCaseTaskController(caseTaskSvc)
+
+    entityLetterRepo       := repository.NewEntityLetterRepository(gormDB)
+    entityLetterSvc        := service.NewEntityLetterService(entityLetterRepo, caseTimelineRepo, caseTaskRepo)
+    entityLetterCtrl       := salvia_ctrl.NewEntityLetterController(entityLetterSvc)
 
     // Routes
     api := router.Group("/api/v1")
